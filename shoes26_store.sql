436b30a83c9faaa1b3be916bcd5482a987dfebbe
thuan711 <thuanhvps24572@fpt.edu.vn>
2022-11-18 23:04:51 +0700

    a

--- shoes26_store.sql
+++ shoes26_store.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 15, 2022 at 06:04 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 18, 2022 lúc 05:03 PM
+-- Phiên bản máy phục vụ: 10.4.24-MariaDB
+-- Phiên bản PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `shoes26_store`
+-- Cơ sở dữ liệu: `shoes26_store`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `binh_luan`
+-- Cấu trúc bảng cho bảng `binh_luan`
 --
 
 CREATE TABLE `binh_luan` (
@@ -38,7 +38,7 @@ CREATE TABLE `binh_luan` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `chi_tiet_don_hang`
+-- Cấu trúc bảng cho bảng `chi_tiet_don_hang`
 --
 
 CREATE TABLE `chi_tiet_don_hang` (
@@ -52,7 +52,7 @@ CREATE TABLE `chi_tiet_don_hang` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `don_hang`
+-- Cấu trúc bảng cho bảng `don_hang`
 --
 
 CREATE TABLE `don_hang` (
@@ -72,7 +72,7 @@ CREATE TABLE `don_hang` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `hang_hoa`
+-- Cấu trúc bảng cho bảng `hang_hoa`
 --
 
 CREATE TABLE `hang_hoa` (
@@ -84,27 +84,28 @@ CREATE TABLE `hang_hoa` (
   `mota` text COLLATE utf8_unicode_ci NOT NULL,
   `iddm` int(10) NOT NULL,
   `luotxem` int(11) NOT NULL DEFAULT 0,
-  `trang_thai` tinyint(1) NOT NULL DEFAULT 1
+  `trang_thai` tinyint(1) NOT NULL DEFAULT 1,
+  `special` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `hang_hoa`
+-- Đang đổ dữ liệu cho bảng `hang_hoa`
 --
 
-INSERT INTO `hang_hoa` (`id`, `name`, `price_old`, `price_new`, `img`, `mota`, `iddm`, `luotxem`, `trang_thai`) VALUES
-(101, 'Ralph Sampson', 100.00, 0.00, 'puma-Ralph Sampson.jpg', 'fdf', 38, 0, 1),
-(103, 'Vans Classic Slip On ', 57.00, 0.00, 'vans-classic-slipon.jpg', 'Classic Slipon', 39, 0, 1),
-(104, 'ADIDAS HYPERTURF', 167.00, 0.00, 'adidas-HYPERTURF.jpg', 'HYPERTURF', 37, 0, 1),
-(105, 'Puma RS-X', 130.00, 0.00, 'puma-RS-X.jpg', 'PUMA x TMC RS-X Sneakers', 38, 0, 1),
-(106, 'Jordan Series Mid', 99.00, 0.00, 'Jordan Series Mid.jpg', 'Nike Jordan Series Mid', 36, 0, 1),
-(112, 'VANS VN0A5', 83.00, 0.00, 'vans VN0A5KRDBZW.jpg', 'Vans VN0A5KRDBZW thuộc dòng Authentic Collage Black/White mới nhất trong bộ sưu tập của thương hiệu Vans', 39, 0, 1),
-(113, 'SuperStart', 99.00, 0.00, 'superstart1.jpg', 'z', 37, 0, 1),
-(114, 'LeBron 19', 124.00, 0.00, 'LeBron 19.jpg', '', 36, 0, 0);
+INSERT INTO `hang_hoa` (`id`, `name`, `price_old`, `price_new`, `img`, `mota`, `iddm`, `luotxem`, `trang_thai`, `special`) VALUES
+(101, 'Ralph Sampson', 100.00, 0.00, 'puma-Ralph Sampson.jpg', 'fdf', 38, 0, 1, 0),
+(103, 'Vans Classic Slip On ', 57.00, 0.00, 'vans-classic-slipon.jpg', 'Classic Slipon', 39, 0, 1, 0),
+(104, 'ADIDAS HYPERTURF', 167.00, 0.00, 'adidas-HYPERTURF.jpg', 'HYPERTURF', 37, 0, 1, 0),
+(105, 'Puma RS-X', 130.00, 0.00, 'puma-RS-X.jpg', 'Mẫu Sneaker Puma RSX là một trong những thiết kế huyền thoại của Puma. Đây là mẫu giày Chunky được đánh giá là xuất sắc nhất của hãng, thu hút được sự yêu thích của đông đảo giới trẻ. Đây thực sự là mẫu giày đã góp phần làm nên tên tuổi của Puma như hiện tại.', 38, 0, 1, 1),
+(106, 'Jordan Series Mid', 99.00, 0.00, 'Jordan Series Mid.jpg', 'Nike Jordan Series Mid', 36, 0, 1, 0),
+(112, 'VANS VN0A5', 83.00, 0.00, 'vans VN0A5KRDBZW.jpg', 'Vans VN0A5KRDBZW thuộc dòng Authentic Collage Black/White mới nhất trong bộ sưu tập của thương hiệu Vans', 39, 0, 1, 0),
+(113, 'SuperStart', 99.00, 0.00, 'superstart1.jpg', 'z', 37, 0, 1, 0),
+(114, 'LeBron 19', 124.00, 0.00, 'LeBron 19.jpg', '', 36, 0, 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `hinh_anh`
+-- Cấu trúc bảng cho bảng `hinh_anh`
 --
 
 CREATE TABLE `hinh_anh` (
@@ -116,7 +117,7 @@ CREATE TABLE `hinh_anh` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `loai`
+-- Cấu trúc bảng cho bảng `loai`
 --
 
 CREATE TABLE `loai` (
@@ -125,7 +126,7 @@ CREATE TABLE `loai` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `loai`
+-- Đang đổ dữ liệu cho bảng `loai`
 --
 
 INSERT INTO `loai` (`ma_loai`, `ten_loai`) VALUES
@@ -137,7 +138,7 @@ INSERT INTO `loai` (`ma_loai`, `ten_loai`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ma_giam_gia`
+-- Cấu trúc bảng cho bảng `ma_giam_gia`
 --
 
 CREATE TABLE `ma_giam_gia` (
@@ -149,7 +150,7 @@ CREATE TABLE `ma_giam_gia` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tai_khoan`
+-- Cấu trúc bảng cho bảng `tai_khoan`
 --
 
 CREATE TABLE `tai_khoan` (
@@ -165,7 +166,7 @@ CREATE TABLE `tai_khoan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `tai_khoan`
+-- Đang đổ dữ liệu cho bảng `tai_khoan`
 --
 
 INSERT INTO `tai_khoan` (`ma_tk`, `img`, `ho_ten`, `user`, `pass`, `email`, `address`, `tel`, `role`) VALUES
@@ -175,7 +176,7 @@ INSERT INTO `tai_khoan` (`ma_tk`, `img`, `ho_ten`, `user`, `pass`, `email`, `add
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tin_tuc`
+-- Cấu trúc bảng cho bảng `tin_tuc`
 --
 
 CREATE TABLE `tin_tuc` (
@@ -187,18 +188,18 @@ CREATE TABLE `tin_tuc` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `binh_luan`
+-- Chỉ mục cho bảng `binh_luan`
 --
 ALTER TABLE `binh_luan`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_bl_hh` (`idpro`);
 
 --
--- Indexes for table `chi_tiet_don_hang`
+-- Chỉ mục cho bảng `chi_tiet_don_hang`
 --
 ALTER TABLE `chi_tiet_don_hang`
   ADD PRIMARY KEY (`ma_ctdh`),
@@ -206,133 +207,133 @@ ALTER TABLE `chi_tiet_don_hang`
   ADD KEY `fk_ctdh_hh` (`ma_hh`);
 
 --
--- Indexes for table `don_hang`
+-- Chỉ mục cho bảng `don_hang`
 --
 ALTER TABLE `don_hang`
   ADD PRIMARY KEY (`ma_dh`),
   ADD KEY `fk_dh_tk` (`ma_tk`);
 
 --
--- Indexes for table `hang_hoa`
+-- Chỉ mục cho bảng `hang_hoa`
 --
 ALTER TABLE `hang_hoa`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_hh_lh` (`iddm`);
 
 --
--- Indexes for table `hinh_anh`
+-- Chỉ mục cho bảng `hinh_anh`
 --
 ALTER TABLE `hinh_anh`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_ha_hh` (`ma_hh`);
 
 --
--- Indexes for table `loai`
+-- Chỉ mục cho bảng `loai`
 --
 ALTER TABLE `loai`
   ADD PRIMARY KEY (`ma_loai`);
 
 --
--- Indexes for table `ma_giam_gia`
+-- Chỉ mục cho bảng `ma_giam_gia`
 --
 ALTER TABLE `ma_giam_gia`
   ADD PRIMARY KEY (`ma_gg`);
 
 --
--- Indexes for table `tai_khoan`
+-- Chỉ mục cho bảng `tai_khoan`
 --
 ALTER TABLE `tai_khoan`
   ADD PRIMARY KEY (`ma_tk`);
 
 --
--- Indexes for table `tin_tuc`
+-- Chỉ mục cho bảng `tin_tuc`
 --
 ALTER TABLE `tin_tuc`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `binh_luan`
+-- AUTO_INCREMENT cho bảng `binh_luan`
 --
 ALTER TABLE `binh_luan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `chi_tiet_don_hang`
+-- AUTO_INCREMENT cho bảng `chi_tiet_don_hang`
 --
 ALTER TABLE `chi_tiet_don_hang`
   MODIFY `ma_ctdh` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `don_hang`
+-- AUTO_INCREMENT cho bảng `don_hang`
 --
 ALTER TABLE `don_hang`
   MODIFY `ma_dh` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `hang_hoa`
+-- AUTO_INCREMENT cho bảng `hang_hoa`
 --
 ALTER TABLE `hang_hoa`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
 
 --
--- AUTO_INCREMENT for table `hinh_anh`
+-- AUTO_INCREMENT cho bảng `hinh_anh`
 --
 ALTER TABLE `hinh_anh`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- AUTO_INCREMENT for table `loai`
+-- AUTO_INCREMENT cho bảng `loai`
 --
 ALTER TABLE `loai`
   MODIFY `ma_loai` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Mã loại hàng', AUTO_INCREMENT=45;
 
 --
--- AUTO_INCREMENT for table `tai_khoan`
+-- AUTO_INCREMENT cho bảng `tai_khoan`
 --
 ALTER TABLE `tai_khoan`
   MODIFY `ma_tk` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1952;
 
 --
--- AUTO_INCREMENT for table `tin_tuc`
+-- AUTO_INCREMENT cho bảng `tin_tuc`
 --
 ALTER TABLE `tin_tuc`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
--- Constraints for dumped tables
+-- Các ràng buộc cho các bảng đã đổ
 --
 
 --
--- Constraints for table `binh_luan`
+-- Các ràng buộc cho bảng `binh_luan`
 --
 ALTER TABLE `binh_luan`
   ADD CONSTRAINT `fk_bl_hh` FOREIGN KEY (`idpro`) REFERENCES `hang_hoa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `chi_tiet_don_hang`
+-- Các ràng buộc cho bảng `chi_tiet_don_hang`
 --
 ALTER TABLE `chi_tiet_don_hang`
   ADD CONSTRAINT `fk_ctdh_dh` FOREIGN KEY (`ma_dh`) REFERENCES `don_hang` (`ma_dh`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_ctdh_hh` FOREIGN KEY (`ma_hh`) REFERENCES `hang_hoa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `don_hang`
+-- Các ràng buộc cho bảng `don_hang`
 --
 ALTER TABLE `don_hang`
   ADD CONSTRAINT `fk_dh_tk` FOREIGN KEY (`ma_tk`) REFERENCES `tai_khoan` (`ma_tk`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `hang_hoa`
+-- Các ràng buộc cho bảng `hang_hoa`
 --
 ALTER TABLE `hang_hoa`
   ADD CONSTRAINT `fk_hh_lh` FOREIGN KEY (`iddm`) REFERENCES `loai` (`ma_loai`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `hinh_anh`
+-- Các ràng buộc cho bảng `hinh_anh`
 --
 ALTER TABLE `hinh_anh`
   ADD CONSTRAINT `fk_ha_hh` FOREIGN KEY (`ma_hh`) REFERENCES `hang_hoa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
