af4fe1ba56d2c3f08f8483d2b71b341057557a45
TanDuy <tanduy@gmail.com>
2022-11-21 17:07:46 +0700

    up

--- admin/bill/chitiet_dh.php
+++ admin/bill/chitiet_dh.php
@@ -11,43 +11,51 @@
 				</div>
 			</div>
 		</div>
-        
-	</section><div class="container-fluid">
-        <div class="row-title text-center" style="margin-top:20px;">
-            <h3>Chi tiết đơn hàng:</h3>
-        </div>
-        <div class="table-responsive-sm">
-            <table class="table table-bordered">
-                <thead class="thead-light">
-                    <tr>
-                        <th></th>
-                        <th style="width:360px;">Mã đơn hàng</th>
-                        <th style="width:360px;">Tên sản phẩm</th>
-                        <th style="width:360px;">Giá</th>
-                        <th style="width:360px;">Số lượng</th>
-                        <th style="width:360px;">Size</th>
-                        <th style="width:360px;">Thành tiền</th>
-                        
-                    </tr>
-                </thead>
-                 <?php
-                    foreach ($listchitietdh as $bill) {
-                        extract($bill);
-                        echo '
-                        <tbody>
-                            <tr>
-                                <td><input type="checkbox" name="" id=""></td>
-                                <td>'.$ma_dh.'</td>
-                                <td>'.$name.'</td>
-                                <td>'.$price_old.'$</td>
-                                <td>'.$quantity.'</td>
-                                <td>'.$size.'</td>
-                                <td>'.$tong.'$</td>
-                            </tr>
-                        </tbody>';
-                    }
-                ?> 
-            </div>
-        </div>
-    
-	
+	</section>
+	<!-- End banner Area -->
+<div class="container-fluid">
+    <div class="row-title text-center" style="margin-top:20px;">
+        <h3>Chi tiết</h3>
+    </div>
+    <div class="table-responsive-sm">
+        <!-- <div class="d-flex ">
+            <input type="submit" value="Select All" name="" class="form-control " style=" width:120px;background: linear-gradient(131deg, rgba(255,117,0,1) 12%, rgba(255,184,0,1) 86%); color:#fff;">
+            <a href="index.php?act=addtk"><input type="submit" value="Thêm mới" class="btn btn-default border-0" style="margin:0 0 15px 15px; width:120px; background: linear-gradient(131deg, rgba(255,117,0,1) 12%, rgba(255,184,0,1) 86%); color:#fff;"></a>
+            <input type="submit" value="Chỉnh sửa" class="btn btn-default border-0" style="margin:0 0 15px 15px; width:120px; background: linear-gradient(131deg, rgba(255,117,0,1) 12%, rgba(255,184,0,1) 86%); color:#fff;">
+        </div> -->
+        <table class="table table-bordered">
+            <thead class="thead-light">
+                <tr>
+                    <th></th>
+                    <th>Mã đơn hàng</th>
+                    <th>Tên sản phẩm</th>
+                    <th>Số lượng</th>
+                    <th>Giá</th>
+                    <th>Size</th>
+                    <th>Thành tiền</th>
+                </tr>
+            </thead>
+            <?php
+                foreach ($listchitietdh as $bill) {
+                    extract($bill);
+                    // $xoatk="index.php?act=xoatk&id=".$id;
+                    // $suatk="index.php?act=suatk&id=".$id;
+                    echo '
+                    <tbody>
+                        <tr>
+                            <td><input type="checkbox" name="" id=""></td>
+                            <td>'.$ma_dh.'</td>
+                            <td>'.$name.'</td>
+                            <td>'.$price_old.'</td>
+                            <td>'.$quantity.'</td>
+                            <td>'.$size.'</td>
+                            <td>'.$tong.'</td>
+                        </tr>
+                    </tbody>';
+                }
+            ?>
+        </table>
+    </div>
+</div>
+<!--  -->
+
