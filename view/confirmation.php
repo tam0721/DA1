1d07a972805b0e8e29c99756d1697a11b5083b61
thuan711 <thuanhvps24572@fpt.edu.vn>
2022-12-03 01:48:10 +0700

    b

--- view/confirmation.php
+++ view/confirmation.php
@@ -62,7 +62,7 @@
                                         $ttien = 0;
                                         $ttien += $tong;
                                         $all +=$ttien;
-										$tien=$all+20000;
+										$tien=$all+20;
                                         $del="index.php?act=delcart&idsp=".$ma_hh;
                                         $upd="index.php?act=cart&id=".$ma_hh;
                                         $up="index.php?act=cartupdate&id=".$ma_hh;
@@ -176,7 +176,7 @@
 								<td>
 									<p>
 										<?php
-										echo "$$tien.00";
+										echo "$tien.000 ₫";
 										?>
 									</p>
 								</td>
